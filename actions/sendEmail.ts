555d4999d58e25c76d8feb60eebfb7fd67a49012
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-15 18:33:16 +0530

    new commit

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,8 +26,8 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
+      from: 'onboarding@resend.dev',
+      to: 'muhammadshakil1968@gmail.com',
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Bcom Computer Application",
+    location: "Wayanad,Kerala",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    "Graduated in BCom Computer Application from Wayanad, Kerala in 2020, cherishing a journey of challenges, triumphs, and academic growth.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2020",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Accountant",
+    location: "UAE,Dubai",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Accountant in Dubai, UAE, with 9 months experience, upskilled in Accounting and Finance in 2022-2023.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2022 - 2023",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Full-Stack Developer-Intern",
+    location: "Calicut,Kerala",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "As a Full-Stack Developer intern based in Calicut, Kerala, I bring a robust skill set that includes expertise in React, Next.js, Tailwind, Express, and MongoDB.I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - present",
   },
 ] as const;
 
