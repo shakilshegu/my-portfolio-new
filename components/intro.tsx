ba1f4f90cb6b175f7a51aa539dd2b158f40c16c7
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-15 15:05:49 +0530

    new commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,10 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Muhammed Shakil.</span> I'm a  passionate {" "}
+        <span className="font-bold">MERN stack developer</span> with{" "}
+        <span className="font-bold"></span> a keen interest in crafting engaging and responsive web applications <span className="italic">sites & apps</span>
+        
       </motion.h1>
 
       <motion.div
